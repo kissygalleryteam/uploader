0d30b78f04993d4ac625d923f4ad019fdd2e0d4e
minghe <minghe36@126.com>
2013-05-06 21:37:21 +0800

    add modules.js

---
+++ 1.4/meta/modules.js
@@ -0,0 +1,17 @@
+(function(S){
+    var preMod = "gallery/uploader/1.4/";
+    var plugins = ['auth','filedrop','imageZoom','preview','proBars','tagConfig','urlsInput','ajbridge'];
+    var mods = S.map(plugins,function(plu){
+        return preMod+"plugins/"+plu+'/'+plu;
+    })
+    var otherMods = ['gallery/flash/1.0/index',preMod+'plugins/proBars/progressBar',preMod+'plugins/ajbridge/uploader',preMod+'theme'];
+    S.each(otherMods,function(mod){
+        mods.push(mod);
+    })
+
+    S.config({
+        modules:{
+            'gallery/uploader/1.4/index':{requires:mods}
+        }
+    })
+})(KISSY);
