f35ae553cab43ff46b8d03a7e13020b3be66f08e
jianping.xwh <jianping.xwh@taobao.com>
2013-11-03 15:52:21 +0800

    fix #114

--- 1.5/build/plugins/coverPic/coverPic-min.js
+++ 1.5/build/plugins/coverPic/coverPic-min.js
@@ -1,2 +1,2 @@
-/*!build time : 2013-10-24 4:46:27 PM*/
+/*!build time : 2013-11-03 3:51:10 PM*/
 KISSY.add("gallery/uploader/1.5/plugins/coverPic/coverPic",function(a,b,c){function d(){}return b.all,a.extend(d,c,{pluginInitializer:function(a){return a?void 0:!1}},{ATTRS:{pluginId:{value:"coverPic"}}}),d},{requires:["node","base"]});
