aef4b76ea5986709cad0e9b3403420ebd54dd10f
剑平 <jianping.xwh@taobao.com>
2014-02-13 11:14:26 +0800

    修正文档

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,42 @@
 module.exports = function(grunt) {
-	var task = grunt.task;
+    var configs = grunt.file.readJSON('abc.json');
+    var BUILD = 'build/';
+    var versionPath = '1.5/';
+    var kmcSrcs = ['index.js','theme.js','aliUploader.js','nativeUploader.js',
+        ,'plugins/plugins.js','plugins/auth/auth.js','plugins/ajbridge/uploader.js','plugins/coverPic/coverPic.js','plugins/filedrop/filedrop.js','plugins/imageZoom/imageZoom.js','plugins/imgcrop/imgcrop.js','plugins/preview/preview.js','plugins/proBars/proBars.js','plugins/tagConfig/tagConfig.js','plugins/urlsInput/urlsInput.js','plugins/miniLogin/miniLogin.js','plugins/mobileUploader/mobileUploader.js','plugins/paste/paste.js','plugins/cross/cross.js',
+        ,'themes/cropUploader/index.js','themes/daogouUploader/index.js','themes/default/index.js','themes/editorMultipleUploader/index.js','themes/cropUploader/index.js','themes/ershouUploader/index.js','themes/imageUploader/index.js','themes/loveUploader/index.js','themes/mutilImageUploader/index.js','themes/refundUploader/index.js','themes/singleImageUploader/index.js','themes/wankeUploader/index.js','themes/grayUploader/index.js','themes/crossUploader/index.js','themes/nativeUploader/index.js'
+    ];
+    var kmcMain = [];
+    for(var i = 0;i<kmcSrcs.length;i++){
+        var buildPath = versionPath + BUILD + kmcSrcs[i];
+        kmcMain.push({
+            src: versionPath+kmcSrcs[i],
+            dest: buildPath
+        });
+    }
+
+    var themes = ['cropUploader','editorMultipleUploader','imageUploader','refundUploader','singleImageUploader','grayUploader','crossUploader','default','nativeUploader'];
+    var lessMain = {};
+    for(var i = 0;i<themes.length;i++){
+        var lessPath = versionPath + 'themes/' + themes[i] + '/style.less';
+        var buildPath = versionPath + 'build/themes/'+themes[i];
+        var cssPath = buildPath + '/style.css';
+        lessMain[cssPath] = lessPath;
+    }
     grunt.initConfig({
         // 配置文件，参考package.json配置方式，必须设置项是
         // name, version, author
         // name作为gallery发布后的模块名
         // version是版本，也是发布目录
         // author必须是{name: "xxx", email: "xxx"}格式
-        pkg: grunt.file.readJSON('abc.json'),
+        pkg: configs,
+        buildBase: '1.5/build',
         banner: '/*!build time : <%= grunt.template.today("yyyy-mm-dd h:MM:ss TT") %>*/\n',
-
-        // 对build目录进行清理
-        clean: {
-            build: {
-                src: '<%= pkg.version %>/build/*'
-			}
-        },
         // kmc打包任务，默认情况，入口文件是index.js，可以自行添加入口文件，在files下面
         // 添加
         kmc: {
             options: {
+                banner: '<%= banner %>',
                 packages: [
                     {
                         name: '<%= pkg.name %>',
@@ -28,11 +46,13 @@ module.exports = function(grunt) {
                 map: [["<%= pkg.name %>/", "gallery/<%= pkg.name %>/"]]
             },
             main: {
+                files: kmcMain
+            }
+        },
+        copy: {
+            main: {
                 files: [
-                    {
-                        src: "<%= pkg.version %>/index.js",
-                        dest: "<%= pkg.version %>/build/index.js"
-                    }
+                    {src: ['path/**'], dest: 'dest/'}
                 ]
             }
         },
@@ -51,72 +71,33 @@ module.exports = function(grunt) {
                 files: [
                     {
                         expand: true,
-                        cwd: '<%= pkg.version %>/build',
+                        cwd: '<%= buildBase %>',
                         src: ['**/*.js', '!**/*-min.js'],
-                        dest: '<%= pkg.version %>/build',
+                        dest: '<%= buildBase %>',
                         ext: '-min.js'
                     }
                 ]
             }
         },
-		// FlexCombo服务配置
-		// https://npmjs.org/package/grunt-flexcombo
-		flexcombo:{
-			// 无线H5项目调试，可打开host配置，用法参照
-			// https://speakerdeck.com/lijing00333/grunt-flexcombo
-			debug:{
-				options:{
-					proxyport:8080,
-					target:'<%= pkg.version %>/build/',
-					urls:'/s/kissy/gallery/<%= pkg.name %>/<%= pkg.version %>',
-					port:'80',
-					servlet:'?',
-					separator:',',
-					charset:'gbk', // 输出文件的编码
-					// 默认将"-min"文件映射到源文件
-					filter:{
-						'-min\\.js':'.js'
-					}
-				}
-			}
-		},
-		// 拷贝 CSS 文件
-		copy : {
-			main: {
-				files:[
-					{
-						expand:true,
-						cwd:'<%= pkg.version %>/',
-						src: [
-							'**/*.css',
-							'!build/**/*.css',
-							'!demo/**/*.css'
-						], 
-						dest: '<%= pkg.version %>/build/', 
-						filter: 'isFile'
-					}
-				]
-			}
-		},
-		// 监听JS、CSS、LESS文件的修改
-        watch: {
-            'all': {
-                files: [
-					'<%= pkg.version %>/**/*.js',
-					'<%= pkg.version %>/src/**/*.css',
-					'!<%= pkg.version %>/build/**/*'
-				],
-                tasks: [ 'build' ]
+        less: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            themes:{
+                files: lessMain
             }
-		},
+        },
         cssmin: {
-            main: {
+            options: {
+                banner: '<%= banner %>'
+            },
+            themes:{
                 files: [
                     {
                         expand: true,
-                        cwd: '<%= pkg.version %>/build',
+                        cwd: '<%= buildBase %>',
                         src: ['**/*.css', '!**/*-min.css'],
-                        dest: '<%= pkg.version %>/build',
+                        dest: '<%= buildBase %>',
                         ext: '-min.css'
                     }
                 ]
@@ -127,28 +108,7 @@ module.exports = function(grunt) {
     // 使用到的任务，可以增加其他任务
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-kmc');
-    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-copy');
-	grunt.loadNpmTasks('grunt-flexcombo');
-
-	grunt.registerTask('build', '默认构建任务', function() {
-		task.run(['clean:build', 'kmc','uglify', 'copy','cssmin']);
-	});
-
-	// 启动Debug调试时的本地服务
-	grunt.registerTask('debug', '开启debug模式', function() {
-		task.run(['flexcombo:debug','watch:all']);
-	});
-
-	grunt.registerTask('server', '开启debug模式', function() {
-		task.run(['flexcombo:debug','watch:all']);
-	});
-
-    return grunt.registerTask('default', '',function(type){
-		if (!type) {
-			task.run(['build']);
-		}
-	});
+    return grunt.registerTask('default', ['kmc', 'uglify','less','cssmin']);
 };
